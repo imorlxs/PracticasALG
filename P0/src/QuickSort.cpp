8f1b5726079d2bf6edaebc86a39a21267d159506
Isaac Morales <imorsan@correo.ugr.es>
2024-03-01 00:45:49 +0100

    Implementado QuickSort

---
+++ P0/src/QuickSort.cpp
@@ -0,0 +1,132 @@
+/*
+ *  @file QuickSort.cpp
+ *  @author Isaac Morales Santana <imorsan@correo.ugr.es>
+ */
+#include <cstdlib> // Para usar srand y rand
+#include <chrono>
+#include <iostream>
+#include <fstream> // Para usar ficheros
+using namespace std;
+
+void QuickSort(int *v, int n);
+
+
+int main(int argc, char *argv[]) {
+	
+	int *v;
+	int n, i, argumento;
+    chrono::time_point<std::chrono::high_resolution_clock> t0, tf; // Para medir el tiempo de ejecución
+	unsigned long int semilla;
+	ofstream fsalida;
+	
+	if (argc <= 3) {
+		cerr<<"\nError: El programa se debe ejecutar de la siguiente forma.\n\n";
+		cerr<<argv[0]<<" NombreFicheroSalida Semilla tamCaso1 tamCaso2 ... tamCasoN\n\n";
+		return 0;
+	}
+	
+	// Abrimos fichero de salida
+	fsalida.open(argv[1]);
+	if (!fsalida.is_open()) {
+		cerr<<"Error: No se pudo abrir fichero para escritura "<<argv[1]<<"\n\n";
+		return 0;
+	}
+	
+	// Inicializamos generador de no. aleatorios
+	semilla= atoi(argv[2]);
+	srand(semilla);
+	
+	// Pasamos por cada tamaÒo de caso
+	for (argumento= 3; argumento<argc; argumento++) {
+		
+		// Cogemos el tamanio del caso
+		n= atoi(argv[argumento]);
+		
+		// Reservamos memoria para el vector
+		v= new int[n];
+		
+		// Generamos vector aleatorio de prueba, con componentes entre 0 y n-1
+		for (i= 0; i<n; i++)
+			v[i]= rand()%n;
+		
+		cerr << "Ejecutando QuickSort para tam. caso: " << n << endl;
+		
+		t0= std::chrono::high_resolution_clock::now(); // Cogemos el tiempo en que comienza la ejecuciÛn del algoritmo
+		QuickSort(v, n); // Ejecutamos el algoritmo para tamaÒo de caso tam
+		tf= std::chrono::high_resolution_clock::now(); // Cogemos el tiempo en que finaliza la ejecuciÛn del algoritmo
+		
+		unsigned long tejecucion= std::chrono::duration_cast<std::chrono::microseconds>(tf - t0).count();
+		
+		cerr << "\tTiempo de ejec. (us): " << tejecucion << " para tam. caso "<< n<<endl;
+		
+		// Guardamos tam. de caso y t_ejecucion a fichero de salida
+		fsalida<<n<<" "<<tejecucion<<"\n";
+		
+		
+		// Liberamos memoria del vector
+		delete [] v;
+	}
+	
+	// Cerramos fichero de salida
+	fsalida.close();
+	
+	return 0;
+}
+
+/*
+ * Source: https://www.geeksforgeeks.org/cpp-program-for-quicksort/
+ */
+int partition(int arr[], int start, int end)
+{
+ 
+    int pivot = arr[start];
+ 
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
+    }
+ 
+    // Giving pivot element its correct position
+    int pivotIndex = start + count;
+    swap(arr[pivotIndex], arr[start]);
+ 
+    // Sorting left and right parts of the pivot element
+    int i = start, j = end;
+ 
+    while (i < pivotIndex && j > pivotIndex) {
+ 
+        while (arr[i] <= pivot) {
+            i++;
+        }
+ 
+        while (arr[j] > pivot) {
+            j--;
+        }
+ 
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+ 
+    return pivotIndex;
+}
+ 
+void quickSort(int arr[], int start, int end)
+{
+ 
+    // base case
+    if (start >= end)
+        return;
+ 
+    // partitioning the array
+    int p = partition(arr, start, end);
+ 
+    // Sorting the left part
+    quickSort(arr, start, p - 1);
+ 
+    // Sorting the right part
+    quickSort(arr, p + 1, end);
+}
+
+
