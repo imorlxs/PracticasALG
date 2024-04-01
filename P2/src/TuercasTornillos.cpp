845c16f0da80d7408788d789fa7185984d7da41a
LeviDooku <pedrovelascosantana@gmail.com>
2024-04-01 20:49:19 +0200

    Generacion d vectores acabada

--- P2/src/TuercasTornillos.cpp
+++ P2/src/TuercasTornillos.cpp
@@ -10,13 +10,9 @@
 
 using namespace std;
 
-int genera_entero_aleatorio(int menor, int mayor){
+//Función que genera números aleatorios en un rango
 
-    if(menor > mayor){
-        int a = menor;
-        menor = mayor;
-        mayor = a;
-    }
+int genera_entero_aleatorio(int menor, int mayor){
 
     return (rand() % (mayor - menor + 1)) + menor;   
 }
@@ -26,12 +22,18 @@ int main(int argc, char* argv[]){
     
     //Control de los argumentos
 
-    if(argc != 4 || atoi(argv[2]) < 0 || atoi(argv[1]) <= 0 || atoi(argv[3]) <= 0)
+    if(argc != 4 || atoi(argv[2]) < 0 || atoi(argv[1]) <= 0 || atoi(argv[3]) < 0)
         cerr << "[-] Uso: " << argv[0] << " <n de elementos> <valor min (> 0)> <valor max>" << endl;  
 
     int n = atoi(argv[1]);
     int menor = atoi(argv[2]);
     int mayor = atoi(argv[3]);
+
+    if(menor > mayor){
+        int a = menor;
+        menor = mayor;
+        mayor = a;
+    }
 
     if((mayor - menor) < n){
         cerr << "[-] El rango de números debe ser mayor que la cantidad de elementos a almacenar";
@@ -58,9 +60,9 @@ int main(int argc, char* argv[]){
         tuercas[i] = 0;
     }
 
+    //
+
     for(const int& elem : secuencia_base){
-        //Crear variable que decide en que posición se insertará "elem"
-        //en tornillos
         int pos;
 
         do{
