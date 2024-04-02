24410848059ff614b209a14a531a310c92a10d1b
Álvaro <maldonadomedinaalvaro28@gmail.com>
2024-04-02 12:27:29 +0200

    Añadido ejercicio 5

---
+++ P2/src/OrganizarCalendarioCampeonato.cpp
@@ -0,0 +1,32 @@
+/*****************************************************
+ * Álvaro Maldonado Medina
+ * Grupo: 2ºD, subgrupo de prácticas D1
+ * @file OrganizarCalendarioCampeonato.cpp
+ * ¿Usa Divide y Vencerás? --> NO
+*****************************************************/
+
+/**
+ * ENUNCIADO:
+ * 
+ * Se organiza un torneo con n participantes. Cada participante tiene que competir exactamente una vez con
+ * todos los posibles oponentes. Además, cada participante tiene que jugar exactamente un partido cada día. Por
+ * concreción, y sin perdida de generalidad, puede suponerse que las competiciones se celebran en días
+ * sucesivos y que cada participante compite una vez por día. Podemos suponer que el número de participantes
+ * es potencia de dos, lo que nos simplificará el problema (no es necesario que haya jornadas de descanso). Por
+ * lo tanto n = 2^k participantes, con k entero positivo. Se pide construir un calendario que permita que el torneo
+ * concluya en n-1 días
+*/
+
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+
+
+
+
+int main(int argc, char **argv)
+{
+
+}
