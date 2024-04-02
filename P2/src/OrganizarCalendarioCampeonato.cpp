7257d4e9b2cc0458d32c6204800aa95295a97231
Álvaro <maldonadomedinaalvaro28@gmail.com>
2024-04-03 01:14:30 +0200

    Intentado resolver el ejercicio sin Divide y Vencerás, ta chunga la cosa

--- P2/src/OrganizarCalendarioCampeonato.cpp
+++ P2/src/OrganizarCalendarioCampeonato.cpp
@@ -17,16 +17,89 @@
  * concluya en n-1 días
 */
 
-
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <set>
+#include <vector>
+#include <cmath>
+#include <bits/stdc++.h>
+using namespace std;
 
 
+// Función para imprimir el calendario del torneo
+void imprimirCalendario(const vector<vector<pair<int,int>>>& calendario){
+    int dia = 1;
+    for (const auto& partidos : calendario){
+        cout << "Día " << dia++ << ": " << endl;
+        for (const auto& partido : partidos){
+            cout << "Partido " << partido.first << " vs " << partido.second << endl;
+        }
+    }
+
+    cout << endl;
+}
+
+
+// Función que va a construir el calendario del torneo
+vector<vector<pair<int,int>>> construirCalendario(int n){
+
+    // Dado que se trata de un formato liguilla, hay n-1 oponentes disponibles puesto que no
+    // pondremos a jugar a un equipo conta sí mismo, habiendo n/2 partidos a jugar.
+    
+    // Inicializamos el calendario
+    vector<vector<pair<int,int>>> calendario(n-1, vector<pair<int,int>>(n/2));
+
+    // Rellenamos el calendario con los partidos
+    int equipoA, equipoB;
+    for (int dia = 0; dia < n-1; dia++){
+        equipoA = 1;
+        equipoB = n - dia;
+        for (int partido = 0; partido < n/2; partido++){
+            calendario[dia][partido] = make_pair(equipoA++,equipoB++);
+            if (equipoB > n-1)
+                equipoB = 2;
+        }
+    }
+
+    return calendario;
+}
+
+// Comprobar si un número es potencia de 2
+bool esPotenciaDeDos(int n){
+    return n > 0 && (n & (n-1)) == 0;
+}
 
 
 int main(int argc, char **argv)
 {
+    int n;  //nº de partidos
+
+    // Realizamos comprobación sobre el argumento dado (nº de equipos a sortear)
+
+    if (argc != 2)
+    {
+        cerr << "Error, se ha introducido un número de argumentos inválido" << endl;
+        cerr << "Uso del programa: ./OrganizarCalendarioCampeonato <nº de equipos>" << endl;
+        exit(-1);
+    } 
+    else if ( !esPotenciaDeDos( atoi(argv[1]) ) )
+    {
+        cerr << "Error, el número de participantes debe ser una potencia de dos" << endl;
+        exit(-2);
+    }
+    else
+    {
+        n = atoi(argv[1]);
+
+        cout << "[+] Se han introducido " << n << " equipos en la liga, se procede a realizar el sorteo..." << endl;
+
+        // Construcción del calendario
+        vector<vector<pair<int,int>>> calendario = construirCalendario(n);
+        cout << "Calendario del torneo:" << endl;
+        imprimirCalendario(calendario);
+
+    }
+
+    return 0;
 
 }
