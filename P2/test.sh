9fecfbbd70bc940945b17107c2241276fdb0a7cb
LeviDooku <pedrovelascosantana@gmail.com>
2024-04-07 12:40:47 +0200

    makefile + mod test.sh

--- P2/test.sh
+++ P2/test.sh
@@ -25,14 +25,11 @@
 
 #TUERCAS Y TORNILLOS
 
-output_file_tyt_dyv="../dat/test_tyt_dyv.dat"
-output_file_tyt="../dat/test_tyt.dat"
+output_file_tyt_dyv="dat/test_tyt_dyv.dat"
+output_file_tyt="dat/test_tyt.dat"
 
 echo "" > $output_file_tyt
 echo "" > $output_file_tyt_dyv
-
-#g++ -o ../bin/tyt ../src/TuercasTornillos.cpp
-#g++ -o ../bin/tyt_dyv ../src/TuercasTornillosDyV.cpp
 
 #Bucle para las pruebas
 for n in {10..100..10}; do
@@ -41,48 +38,42 @@ for n in {10..100..10}; do
     valor_max=$(($n + 50))
 
     echo "[+] Ejecutando prueba <TuercasTornillos> con n = $n, min = $valor_min, max = $valor_max"
-    ../bin/TuercasTornillos $output_file_tyt $n $valor_min $valor_max
+    bin/TuercasTornillos $output_file_tyt $n $valor_min $valor_max
 
     echo "[+] Ejecutando prueba <TuercasTornillosDyV> con n = $n, min = $valor_min, max = $valor_max"
-    ../bin/TuercasTornillosDyV $output_file_tyt_dyv $n $valor_min $valor_max
+    bin/TuercasTornillosDyV $output_file_tyt_dyv $n $valor_min $valor_max
 done
 
 echo "[+] PRUEBA TUERCAS Y TORNILLOS FINALIZADA"
 
 #Producto de tres elementos
 
-output_file_te_dyv="../dat/test_te_dyv.dat"
-output_file_te="../dat/test_te.dat"
+output_file_te_dyv="dat/test_te_dyv.dat"
+output_file_te="dat/test_te.dat"
 
 # Asegurándose de que los archivos de salida están vacíos al principio
 echo "" > $output_file_te
 echo "" > $output_file_te_dyv
 
-# Compilando los programas C++
-#g++ -o ../bin/te ../src/ProductoTresElementosBasico+Complejo.cpp
-#g++ -o ../bin/te_dyv ../src/ProductoTresElementosDivyVenc.cpp
-
 # Bucle para las pruebas, redirigiendo la salida estándar a los archivos de salida
 for n in {10..100..10}; do
     echo " [+] Ejecutando prueba <ProductoTresElementosBasico+Complejo> con n = $n"
-    ../bin/ProductoTresElementosBasico+Complejo $n >> $output_file_te
+    bin/ProductoTresElementosBasico+Complejo $n >> $output_file_te
 
     echo " [+] Ejecutando prueba <ProductoTresElementosDivyVenc> con n = $n"
-    ../bin/ProductoTresElementosDivyVenc $n >> $output_file_te_dyv
+    bin/ProductoTresElementosDivyVenc $n >> $output_file_te_dyv
 done
 
 echo "[+] PRUEBA TRES ELEMENTOS FINALIZADA"
 
 #Eliminar elementos repetidos
 
-output_file_er_dyv="../dat/test_er_dyv.dat"
-output_file_er="../dat/test_er.dat"
+output_file_er_dyv="dat/test_er_dyv.dat"
+output_file_er="dat/test_er.dat"
 
 echo "" > $output_file_er
 echo "" > $output_file_er_dyv
 
-#g++ -o ../bin/er ../src/EliminarRepetidos.cpp
-#g++ -o ../bin/er_dyv ../src/EliminarRepetidosDyV.cpp
 
 #Bucle para las pruebas
 for n in {10..100..10}; do
@@ -90,34 +81,31 @@ for n in {10..100..10}; do
     valor_min=0
     valor_max=$((n / 2))
     echo "[+] Ejecutando prueba <EliminarRepetidos> con n = $n, min = $valor_min, max = $valor_max"
-    ../bin/EliminarRepetidos $output_file_er $n $valor_min $valor_max
+    bin/EliminarRepetidos $output_file_er $n $valor_min $valor_max
 
     echo "[+] Ejecutando prueba <EliminarRepetidosDyV> con n = $n, min = $valor_min, max = $valor_max"
-    ../bin/EliminarRepetidosDyV $output_file_er_dyv $n $valor_min $valor_max
+    bin/EliminarRepetidosDyV $output_file_er_dyv $n $valor_min $valor_max
 done
 
 echo "[+] PRUEBA ELEMENTOS REPETIDOS FINALIZADA"
 
 #Organización del calendario de un campeonato
 
-output_file_cc_dyv="../dat/test_cc_dyv.dat"
-output_file_cc="../dat/test_cc.dat"
+output_file_cc_dyv="dat/test_cc_dyv.dat"
+output_file_cc="dat/test_cc.dat"
 
 echo "" > $output_file_cc
 echo "" > $output_file_cc_dyv
-
-#g++ -o ../bin/cc ../src/OrganizarCalendarioCampeonato.cpp
-#g++ -o ../bin/cc_dyv ../src/OrganizarCalendarioCampeonatoDyV.cpp
 
 #Bucle para las pruebas
 
 for ((n = 2; n <= 1048576; n *= 2)); do
 
     echo " [+] Ejecutando prueba <OrganizarCalendarioCampeonato> con n = $n"
-    ../bin/OrganizarCalendarioCampeonato $output_file_cc $n
+    bin/OrganizarCalendarioCampeonato $output_file_cc $n
 
     echo "[+] Ejecutando prueba <OrganizarCalendarioCampeonatoDyV> con n = $n"
-    ../bin/OrganizarCalendarioCampeonatoDyV $output_file_cc_dyv $n
+    bin/OrganizarCalendarioCampeonatoDyV $output_file_cc_dyv $n
 done
 
 echo "[+] PRUEBA CALENDARIO CAMPEONATO FINALIZADA"
