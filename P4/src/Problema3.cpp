856cc925965f8f7bf22fd9b8ead1faa4c106d566
Isaac Morales <imorsan@correo.ugr.es>
2024-05-11 18:30:37 +0200

    Terminado Senku

--- P4/src/Problema3.cpp
+++ P4/src/Problema3.cpp
@@ -3,27 +3,35 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include<functional>
 
 using namespace std;
-class Senku{
-  private:
-    int remaining;
-
     class Move {
       public:
         pair<int, int> from;
         pair<int, int> to;
 
         Move(pair<int, int> from_, pair<int,int> to_) : from(from_), to(to_){}
+        Move(){
+          from.first = -1;
+          from.second = -1;
+          to.first = -1;
+          to.second = -1;
+        }
 
         pair<int,int> getPegToRemove() const {
-          return make_pair(from.first + to.first) / 2, (from.second + to.second / 2);
+          return make_pair((from.first + to.first) / 2, (from.second + to.second) / 2);
         }
     };
-
+  std::unordered_map<std::size_t, std::pair<Move, bool>> memory;
+class Senku{
   public:
+    vector<vector<int>> board;
+    int remaining;
+
+
     bool hasWon(){
-      return this.remaining == 1 && tablero[3][3] == 1;
+      return this->remaining == 1 && board[3][3] == 1;
     }
 
   vector<Move> getPossibleMoves() {
@@ -57,16 +65,20 @@ class Senku{
   }
 
   Senku move(Move move){
-    auto newBoard = board;
-    auto toRemove = move.getPegToRemove();
-    newBoard[move.from.first][move.from.second] = 0;
-    newBoard[toRemove.first][toRemove.second] = 0;
-    newBoard[move.to.first][move.to.second] = 1;
+    if (move.from.first != -1){
 
-    return Senku(newBoard, remainingPegs - 1);
+      auto newBoard = board;
+      auto toRemove = move.getPegToRemove();
+      newBoard[move.from.first][move.from.second] = 0;
+      newBoard[toRemove.first][toRemove.second] = 0;
+      newBoard[move.to.first][move.to.second] = 1;
+      return Senku(newBoard, remaining - 1);
+    }
+    else return *this;
+
   }
 
-  Senku(const vector<vector<int>>& initialBoard, int pegsRemaining) : board(initialBoard), pegsRemaining(remainingPegs) {}
+  Senku(const vector<vector<int>>& initialBoard, int pegsRemaining) : board(initialBoard), remaining(pegsRemaining) {}
   Senku(){
         board = {{-1,-1,1,1,1,-1,-1},
                          {-1,-1,1,1,1,-1,-1},
@@ -75,12 +87,12 @@ class Senku{
                          {1,1,1,1,1,1,1},
                          {-1,-1,1,1,1,-1,-1},
                          {-1,-1,1,1,1,-1,-1}};
-        this->pegsRemaining = 33;
+        this->remaining = 32;
 
   }
 
-Pair<Move*, bool> dfs(Solitaire game) {
-    if (game.hasWon()) return make_pair(nullptr, true);
+  pair<Move, bool> dfs(Senku game) {
+    if (game.hasWon()) return make_pair(Move(), true);
 
     // Leer de la memoria
     auto hash = game.getBoardHash();
@@ -88,22 +100,49 @@ Pair<Move*, bool> dfs(Solitaire game) {
         return memory[hash];
 
     auto moves = game.getPossibleMoves();
-    if (moves.empty()) return make_pair(nullptr, false);
+    if (moves.empty()) return make_pair(Move(), false);
 
     // Jugar cada movimiento y llamar recursivamente. Elegir la primera solución encontrada.
     for (auto move : moves) {
         auto newGame = game.move(move);
         auto hasWon = dfs(newGame).second;
-
+        //newGame.toString();
         // Devolver movimiento que llevó a la victoria y almacenar en la memoria
         if (hasWon) {
-            memory[hash] = make_pair(move, true);
-            return memory[hash];
+          cout << "HA GANADO" << endl;
+          memory[hash] = make_pair(move, true);
+          return memory[hash];
         }
+
     }
 
     // Caso en que no se encontró ninguna solución.
-    return std::make_pair(&moves[0], false);
+    return std::make_pair(moves[0], false);
+}
+
+ size_t getBoardHash() const {
+        std::hash<std::string> hasher;
+        std::string boardString;
+        for (const auto& row : board) {
+            for (int cell : row) {
+                boardString += std::to_string(cell);
+            }
+        }
+        return hasher(boardString);
+    }
+
+void toString(){
+  for (int i = 0; i < board.size(); i++){
+    for (int j = 0; j < board.size(); j++){
+      if (board[i][j] == 0)
+        cout << "-" << " ";
+      else if (board[i][j] == 1)
+        cout << "O" << " ";
+      else 
+        cout << " " << " ";
+    }
+    cout << endl;
+  }
 }
 
 
@@ -112,11 +151,13 @@ Pair<Move*, bool> dfs(Solitaire game) {
 int main(){
   Senku game;
      auto startTime = std::chrono::steady_clock::now();
-    int counter = 0;
+    int counter = 0; 
     while (!game.hasWon()) {
         counter++;
         auto move = game.dfs(game);
-        game = game.move(*(move.first));
+        game = game.move((move.first));
+        game.toString();
+        cout << "Quedan: " << game.remaining << " pegs " << endl;
     }
     auto endTime = std::chrono::steady_clock::now();
 
