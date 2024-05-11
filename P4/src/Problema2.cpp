4ba0a9afd5025c527f668d764f7c05a72ffe2a31
Álvaro <maldonadomedinaalvaro28@gmail.com>
2024-05-11 20:49:29 +0200

    pinga

--- P4/src/Problema2.cpp
+++ P4/src/Problema2.cpp
@@ -4,34 +4,34 @@
 
 using namespace std;
 
-// Function to calculate the total convenience of a seating arrangement
-int calculateConvenience(const vector<vector<int>>& convenienceMatrix, const vector<int>& seatingArrangement) {
-    int totalConvenience = 0;
-    int n = seatingArrangement.size();
+// Función para calcular la conveniencia total de la asignación de asientos obtenida
+int calcularConveniencia(const vector<vector<int>>& matrizConveniencia, const vector<int>& ordenAsientos) {
+    int convenienciaTotal = 0;
+    int n = ordenAsientos.size();
     for (int i = 0; i < n; i++) {
-        int leftNeighbor = (i - 1 + n) % n;
-        int rightNeighbor = (i + 1) % n;
-        totalConvenience += convenienceMatrix[seatingArrangement[i]][seatingArrangement[leftNeighbor]];
-        totalConvenience += convenienceMatrix[seatingArrangement[i]][seatingArrangement[rightNeighbor]];
+        int vecinoIzqda = (i - 1 + n) % n;
+        int vecinoDcha = (i + 1) % n;
+        convenienciaTotal += matrizConveniencia[ordenAsientos[i]][ordenAsientos[vecinoIzqda]];
+        convenienciaTotal += matrizConveniencia[ordenAsientos[i]][ordenAsientos[vecinoDcha]];
     }
-    return totalConvenience;
+    return convenienciaTotal;
 }
 
-// Backtracking function to find the optimal seating arrangement
-void backtrack(vector<vector<int>>& convenienceMatrix, vector<int>& seatingArrangement, int currentGuest, int maxConvenience, int& bestConvenience, vector<int>& bestSeatingArrangement) {
-    int n = convenienceMatrix.size();
-    if (currentGuest == n) {
-        int totalConvenience = calculateConvenience(convenienceMatrix, seatingArrangement);
-        if (totalConvenience > bestConvenience) {
-            bestConvenience = totalConvenience;
-            bestSeatingArrangement = seatingArrangement;
+// Función principal del algoritmo de backtracking que busca la asignación de asientos óptima
+void backtrack(vector<vector<int>>& matrizConveniencia, vector<int>& ordenAsientos, int invitadoActual, int convenienciaMaxima, int& mejorConveniencia, vector<int>& mejorAsignacionAsientos) {
+    int n = matrizConveniencia.size();
+    if (invitadoActual == n) {
+        int convenienciaTotal = calcularConveniencia(matrizConveniencia, ordenAsientos);
+        if (convenienciaTotal > mejorConveniencia) {
+            mejorConveniencia = convenienciaTotal;
+            mejorAsignacionAsientos = ordenAsientos;
         }
     } else {
         for (int i = 0; i < n; i++) {
-            if (find(seatingArrangement.begin(), seatingArrangement.end(), i) == seatingArrangement.end()) {
-                seatingArrangement.push_back(i);
-                backtrack(convenienceMatrix, seatingArrangement, currentGuest + 1, maxConvenience, bestConvenience, bestSeatingArrangement);
-                seatingArrangement.pop_back();
+            if (find(ordenAsientos.begin(), ordenAsientos.end(), i) == ordenAsientos.end()) {
+                ordenAsientos.push_back(i);
+                backtrack(matrizConveniencia, ordenAsientos, invitadoActual + 1, convenienciaMaxima, mejorConveniencia, mejorAsignacionAsientos);
+                ordenAsientos.pop_back();
             }
         }
     }
@@ -39,23 +39,25 @@ void backtrack(vector<vector<int>>& convenienceMatrix, vector<int>& seatingArran
 
 int main() {
     int n = 4;
-    vector<vector<int>> convenienceMatrix = {
+    vector<vector<int>> matrizConveniencia = {
         {0, 10, 20, 30},
         {10, 0, 15, 25},
         {20, 15, 0, 30},
         {30, 25, 30, 0}
     };
 
-    vector<int> seatingArrangement;
-    int bestConvenience = 0;
-    vector<int> bestSeatingArrangement;
+    vector<int> ordenAsientos;
+    int mejorConveniencia = 0;
+    int primerInvitado = 0;
+    int convenienciaMaxima = 0;
+    vector<int> mejorAsignacionAsientos;
 
-    backtrack(convenienceMatrix, seatingArrangement, 0, 0, bestConvenience, bestSeatingArrangement);
+    backtrack(matrizConveniencia, ordenAsientos, primerInvitado, convenienciaMaxima, mejorConveniencia, mejorAsignacionAsientos);
 
-    cout << "Maximum convenience: " << bestConvenience << endl;
+    cout << "Maximum convenience: " << mejorConveniencia << endl;
     cout << "Optimal seating arrangement: ";
-    for (int i = 0; i < bestSeatingArrangement.size(); i++) {
-        cout << bestSeatingArrangement[i] + 1 << " ";
+    for (int i = 0; i < mejorAsignacionAsientos.size(); i++) {
+        cout << mejorAsignacionAsientos[i] + 1 << " ";
     }
     cout << endl;
 
